9ca98ef5f100e1310fd7678699abe94ecd5f93bd
danielOuattara <daniel.ouattara@gmx.com>
2023-08-03 10:49:44 +0200

    Add business Layer

--- src/main/java/com/ouattara/daniel/student/StudentController.java
+++ src/main/java/com/ouattara/daniel/student/StudentController.java
@@ -12,12 +12,15 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
 
     @GetMapping
-    public List<Student> hello() {
-        return List.of(
-                new Student(1L, "Mariam Jones", "mariamjones@email.com", LocalDate.of(2001, Month.DECEMBER, 15), 22),
-                new Student(2L, "John Doe", "johndoe@email.com", LocalDate.of(1970, Month.JANUARY, 1), 53)
-        );
+    public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 }
---
+++ src/main/java/com/ouattara/daniel/student/StudentService.java
@@ -0,0 +1,17 @@
+package com.ouattara.daniel.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1L, "Mariam Jones", "mariamjones@email.com", LocalDate.of(2001, Month.DECEMBER, 15), 22),
+                new Student(2L, "John Doe", "johndoe@email.com", LocalDate.of(1970, Month.JANUARY, 1), 53)
+        );
+    }
+}
